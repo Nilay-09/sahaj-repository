bd9ac2b2a1fff7b6ef054e9b7e20c87d30ab9805
Abhinav <abhinavmahalley22@gmail.com>
2022-07-30 19:58:52 +0530

    combine files

--- Patient/patient.js
+++ Patient/patient.js
@@ -2,35 +2,40 @@ let historyCont = document.querySelector('.history');
 let diseaseCont = document.querySelector('.disease');
 let insuranceCont = document.querySelector('.insurance');
 let classList = document.querySelectorAll(".list-group-item");
-console.log(classList);
+// console.log(classList);
 
 function history(e){
     historyCont.style.display = "block";
     diseaseCont.style.display = "none";
     insuranceCont.style.display = "none";
-    console.log(e);
-    activeBtn(e);
+    // console.log();
+    activeBtn();
     e.classList.add("active")
+    // e.classList.add("active")
 
 }
-function insurance(){
+function insurance(e){
     historyCont.style.display = "none";
     diseaseCont.style.display = "none";
     insuranceCont.style.display = "block";
+    activeBtn();
+    e.classList.add("active")
 }
 
-function disease(){
+function disease(e){
     historyCont.style.display = "none";
     diseaseCont.style.display = "block";
     insuranceCont.style.display = "none";
+    activeBtn();
+    e.classList.add("active")
 }
 
 function activeBtn(){
    
-    classList.forEach(box => {
+    classList.forEach(act => {
     // ✅ Remove class from each element
-    if(classList.classList.contains('active')){
-        classList.classList.remove('active');
+    if(act.classList.contains('active')){
+        act.classList.remove('active');
     }
   
     // ✅ Add class to each element
